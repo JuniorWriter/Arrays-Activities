f0f38e95a54a5f06b1823cd4347ae91c64445b25
JuniorWriter <62343874+JuniorWriter@users.noreply.github.com>
2020-05-25 16:23:50 -0500

    Punto 5 | Primera actividad

--- ArraysP5-prom.cpp
+++ ArraysP5-prom.cpp
@@ -10,19 +10,19 @@ using namespace std;
 
 int main()
 {
-	float numeros[10];
+	float numeros[10],cantidad=10;
     float suma=0;
 
 	setlocale (LC_CTYPE, "Spanish");
 	
     cout<<"\nIngrese 10 números:\n";
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < cantidad; i++)
     {
         cin>>numeros[i];
         suma+=numeros[i];
     }
 
-	cout<<"\n\nEl promedio de los números es de "<<suma/10<<"\n\n";
+	cout<<"\n\nEl promedio de los números es de "<<suma/cantidad<<"\n\n"; //Dividir entre la cantidad de terminos
 	
 	system("pause");
 	return 0;
