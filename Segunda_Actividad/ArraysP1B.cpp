232d30936021a04bdba99134bbcd605851f1374e
JuniorWriter <62343874+JuniorWriter@users.noreply.github.com>
2020-05-28 14:51:35 -0500

    Primer punto | Primera actividad

---
+++ Segunda_Actividad/ArraysP1B.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <wchar.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+    float numeros[10], mayores[10];
+    float suma, promedio;
+    int i;
+    int opc, size_vector;
+
+    setlocale (LC_CTYPE,"Spanish");
+
+    do
+    {
+        cout<<"Ingrese el tamaño del vector: ";
+        cin>>size_vector;
+
+        cout<<"\n";
+
+        cout<<"Ingrese los "<<size_vector<<" números: \n";
+        for ( i = 0; i < size_vector; i++)
+        {
+            cin>>numeros[i];
+            suma+=numeros[i];
+        }
+        
+        promedio=suma/size_vector;
+
+        cout<<"\nEl promedio de los números ingresados es "<<promedio<<"\n";
+
+        for ( i = 0; i < size_vector; i++)
+        {
+            if (numeros[i]>promedio)
+            {
+                mayores[i]=numeros[i];
+                cout<<"\nEl número "<<mayores[i]<<" es mayor al promedio.";
+            }
+        }
+        
+        /////////////////////////////////////////
+        cout<<"\n\n¿Desea repetir el programa?\n";
+        cout<<"Si.......................[1]\n";
+        cout<<"No.......................[2]\n";
+        cin>>opc;
+
+    } while (opc==1);
+    cout<<"\n";
+
+    system("pause");
+    return 0;
+}
