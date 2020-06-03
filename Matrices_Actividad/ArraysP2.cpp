aa7039958e46675670d8616459ca1521d76bf895
Andres <afgalvan@unicesar.edu.co>
2020-06-02 21:48:54 -0500

    Punto 2 | Actividad matrices

---
+++ Matrices_Actividad/ArraysP2.cpp
@@ -0,0 +1,56 @@
+/*2.Que rellene una matrizcon los 100 primeros números pares y muestre su suma.*/
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <locale.h>
+#include <wchar.h>
+//shortcuts
+#define ERROR system("color 4")
+#define CLEAR system("cls")
+#define PAUSE system("pause")
+#define SPACE getch();
+#define JUMP cout<<"\n";
+
+using namespace std;
+
+int main()
+{
+    int vector[50][50];
+    int suma=0;
+    int filas=10, columnas=10;
+    int f=0, c=0, i=0;
+
+    setlocale (LC_CTYPE,"Spanish");
+
+    JUMP;
+    cout<<"\t\t\t\t\tMATRIZ DE PARES.\n\n";
+
+    for ( f = 0; f < filas; f++)
+    {
+        for ( c = 0; c < columnas; c++)
+        {
+            vector[f][c]=i++*2+2;
+        }
+        
+    }
+
+    for ( f = 0; f < filas; f++)
+    {
+        for ( c = 0; c < columnas; c++)
+        {
+            cout<<"\t"<<vector[f][c];
+            suma+=vector[f][c];
+        }
+        JUMP;
+    }
+    JUMP;
+    
+    cout<<"-------------------------------------------------------------------------------------------";JUMP;
+    cout<<"RESULTADO DE LA SUMA: ";JUMP;
+    cout<<suma;
+
+    JUMP;JUMP;
+    PAUSE;
+    return 0;
+}
